a1e4d15efe1ec2aaae058859561df961e74eb483
Vladimir Benko <vnbenko@gmail.com>
2021-05-24 21:54:13 +0300

    feat: add the logic for LevelOrderTraversalForTree for exercise 4

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,28 @@
 #import "LevelOrderTraversal.h"
 
+typedef enum {
+    left,
+    right
+} Node;
+
+NSInteger createLevelOrderTravelsal(NSArray *tree, NSUInteger index, NSMutableArray *resultTree, NSUInteger level) {
+    
+    for (Node node = left; (level < [tree count]) && (node <= right); node++) {
+        NSObject *node = [tree objectAtIndex:level];
+        level++;
+        if ([node isKindOfClass:NSNumber.class]) {
+            if ([resultTree count] <= index) {
+                [resultTree addObject:[NSMutableArray new]];
+            }
+            [resultTree[index] addObject:node];
+            level = createLevelOrderTravelsal(tree, index + 1, resultTree, level);
+        }
+    }
+    return level;
+}
+
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+    NSMutableArray *resultTree = [NSMutableArray new];
+        createLevelOrderTravelsal(tree, 0, resultTree, 0);
+        return [resultTree copy];
 }
