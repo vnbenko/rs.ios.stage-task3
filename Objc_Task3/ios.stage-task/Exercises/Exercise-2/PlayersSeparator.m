a5f3f7a479986e8b34397f84c520440a3da90ee6
Vladimir Benko <vnbenko@gmail.com>
2021-05-24 21:15:16 +0300

    feat: add the logic for method dividePlayersIntoTeams for exercise 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,6 +3,19 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
+    if (ratingArray.count > 0) {
+        int count = 0;
+        for (int i = 0; i < ratingArray.count - 2; i++) {
+            for (int j = i+1; j < ratingArray.count - 1; j++) {
+                for (int k = j+1; k < ratingArray.count; k++) {
+                    if ((ratingArray[i].intValue < ratingArray[j].intValue) == (ratingArray[j].intValue < ratingArray[k].intValue)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
     return 0;
 }
 
