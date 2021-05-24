660f3e99d81859a1117517b092e0f1101a2831cd
Vladimir Benko <vnbenko@gmail.com>
2021-05-24 21:35:16 +0300

    feat: add the logic for method findLexicographicallyMinimalForString1 for exercise 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,28 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *result = [NSMutableString new];
+    
+    while ([string1 length] && [string2 length]) {
+        const char *s1 = [string1 UTF8String];
+        const char *s2 = [string2 UTF8String];
+        
+        if (s1[0] <= s2[0]) {
+            [result appendString: [string1 substringToIndex: 1]];
+            string1 = [string1 substringFromIndex: 1];
+        } else {
+            [result appendString: [string2 substringToIndex: 1]];
+            string2 = [string2 substringFromIndex: 1];
+        }
+    }
+    
+    if ([string1 length]) {
+        [result appendString: string1];
+    } else {
+        [result appendString: string2];
+    }
+    
+    return result;
 }
 
 @end
